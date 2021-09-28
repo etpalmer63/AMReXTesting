7da151bc7fbd38517766199b429bd7aa84ee7dc6
etpalmer63 <etpalmer@math.sc.edu>
2021-09-28 15:05:26 -0400

    gull path to sh file

--- .github/workflows/id_most_recent_comment_on_PR.sh
+++ .github/workflows/id_most_recent_comment_on_PR.sh
@@ -9,5 +9,6 @@ sudo apt-get install -y --no-install-recommends\
 
 # indetify the most recent comment associated with a PR 
 
-curl  https://api.github.com/repos/etpalmer63/AMReXTesting/issues/comments | jq
+curl   -H "Accept: application/vnd.github.v3+json"   https://api.github.com/repos/etpalmer63/AMReXTesting/issues/comments?direction=desc | jq .[0]
+
 
