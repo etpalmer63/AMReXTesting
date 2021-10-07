3d440c1be2815f9c5fbfc80b34650eea69d0d19f
etpalmer63 <etpalmer@math.sc.edu>
2021-10-07 18:05:16 -0400

    remove unused variables

--- .github/workflows/id_most_recent_comment_on_PR.sh
+++ .github/workflows/id_most_recent_comment_on_PR.sh
@@ -14,21 +14,15 @@ sudo apt-get install -y --no-install-recommends\
 PR_ID=$(curl -H "Accept: application/vnd.github.v3+json"   https://api.github.com/repos/etpalmer63/AMReXTesting/events | jq .[1].payload.issue.number)
 
 
-# identify forked branch from which the 2nd most recent comment came 
+# identify forked branch from PR
 
 CONTENT=$(curl -H "Accept: application/vnd.github.v3+json"   https://api.github.com/repos/etpalmer63/AMReXTesting/pulls/$PR_ID) 
 
-
-REPO_URL=$(jq -r '.head.repo.html_url' <<< "${CONTENT}")
-
-SHA=$(jq -r '.head.sha' <<< "${CONTENT}")
 
 PR_BRANCH=$(jq -r '.head.ref' <<< "${CONTENT}")
 
 echo PR ID Number: $PR_ID
 echo PR Branchname: $PR_BRANCH
-echo Repo: $REPO_URL
-echo sha: $SHA
 
 #Trigger GitLab CI, pass variables
 # succeeds at command line
